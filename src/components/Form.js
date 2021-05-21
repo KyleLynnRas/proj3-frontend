b5fe27c8b3ee4ab9611df82017fc5bf31029c496
Valerie Larson <valerielarsonn@gmail.com>
2021-05-20 19:27:25 -0600

    Set up components/pages boilerplate and started on Main

---
+++ src/components/Form.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Form = (props) => {
+  return <h1>The Form Component</h1>;
+};
+
+export default Form;
---
+++ src/components/Main.js
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import { Route, Switch } from "react-router-dom";
+import Index from "../pages/Index";
+import Show from "../pages/Show";
+import Search from "../pages/Search";
+
+
+function Main(props){
+
+  // const URL "https://proj3-backend.herokuapp.com/jobs";
+
+  const [jobs, setJobs] = useState(null);
+
+  const getJobs = async () => {
+    const response = await fetch(URL);
+    const data = await response.json();
+    setJobs(data);
+    console.log(getJobs)
+  };
+
+// useEffect(() => getJobs(), []);
+
+  return (
+    <main>
+      <Switch>
+        <Route exact path="/jobs">
+          <Index jobs={jobs}/>
+        </Route>
+        <Route 
+          path="/jobs/search"
+          render={(rp) => (
+            <Search
+              {...rp}
+            />
+          )}
+        />
+        <Route
+          path="/jobs/:id"
+          render={(rp) => (
+            <Show
+              {...rp}
+            />
+          )}
+        />
+      </Switch>
+    </main>
+  );
+}
+  
+  export default Main;
