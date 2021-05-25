cc131f7b88d3b4eeeee83b429e1ebf8eefe6e582
KyleLynnRas <kylelynn.ras@gmail.com>
2021-05-25 19:42:37 -0400

    Add ternary to checkboxes in edit form

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -22,26 +22,19 @@ function Edit(props) {
     props.history.push(`/jobs/${id}`);
 }
 
-  const removeJobs = () => {
-    console.log(job._id);
-    props.deleteJobs(job._id);
-    props.history.push(`/jobs`);
-    };
-
-
   return (
       <div className='job'>
         <h1>Edit Form</h1>
         <form onSubmit={handleSubmit}>
           <textarea value={editForm.notes} name="notes" placeholder="Notes" onChange={handleChange}></textarea><br></br>
           <label for="Apply">Applied</label>
-          <input type="checkbox" value={editForm.applied} name="applied" onChange={handleChange}></input><br></br>
+          <input type="checkbox" value={editForm.applied ? false : true} name="applied" onChange={handleChange}></input><br></br>
           <label for="Interview">Interviewed</label>
-          <input type="checkbox" value={editForm.interviewed} name="interviewed" onChange={handleChange}></input><br></br>
+          <input type="checkbox" value={editForm.interviewed ? false : true} name="interviewed" onChange={handleChange}></input><br></br>
           <label for="Cover_letter">Cover Letter</label>
-          <input type="checkbox" value={editForm.cover_letter} name="cover_letter" onChange={handleChange}></input><br></br>
+          <input type="checkbox" value={editForm.cover_letter ? false : true} name="cover_letter" onChange={handleChange}></input><br></br>
           <label for="Resume">Resume</label>
-          <input type="checkbox" value={editForm.resume} name="resume" onChange={handleChange}></input><br></br>
+          <input type="checkbox" value={editForm.resume ? false : true} name="resume" onChange={handleChange}></input><br></br>
           <input type="submit" value="UPDATE" />
         </form>
       </div>
