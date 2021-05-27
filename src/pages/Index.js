8ee3edc07882fbe557b1e63a54b517cb28061f67
KyleLynnRas <kylelynn.ras@gmail.com>
2021-05-26 23:14:48 -0400

    Add style to Index

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,23 +1,31 @@
-import JobCard from "../components/JobCard"
+import JobCard from "../components/JobCard";
 
 function Index(props){
     
   const loaded = () => {
     return props.jobs.map((job) => (
       <JobCard key={job._id} id={job._id} title={job.title} salary={job.salary} candidate_required_location={job.candidate_required_location} type={job.job_type}/>
-    ))
-  }
+    ));
+  };
  
   const loading = () => {
-    return <h1>Loading..</h1>
-  }
+    return(
+      <div className="index-loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  };
   
   return(
     <div className="index-container">
-      <h1>App Title</h1>
-      {props.jobs ? loaded() : loading()}
+      <section className="index-title-container">
+        <h1>npm Start Career</h1>
+      </section>
+      <section className="section">
+        {props.jobs ? loaded() : loading()}
+      </section>
     </div>
-    )
-  } 
+    );
+  };
   
   export default Index;
