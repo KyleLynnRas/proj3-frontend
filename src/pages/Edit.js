71939cfd57fb0e8702b241deb29f8de5c287e1b8
KyleLynnRas <kylelynn.ras@gmail.com>
2021-05-31 20:53:28 -0400

    Add additional checkboxes to edit

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -28,15 +28,27 @@ function Edit(props) {
         <h1>Edit Form</h1>
         <form onSubmit={handleSubmit}>
           <textarea value={editForm.notes} name="notes" placeholder="Notes" onChange={handleChange}></textarea><br></br>
-          <label for="Apply">Applied</label>
-          <input type="checkbox" value={editForm.applied ? false : true} name="applied" onChange={handleChange}></input><br></br>
-          <label for="Interview">Interviewed</label>
-          <input type="checkbox" value={editForm.interviewed ? false : true} name="interviewed" onChange={handleChange}></input><br></br>
-          <label for="Cover_letter">Cover Letter</label>
-          <input type="checkbox" value={editForm.cover_letter ? false : true} name="cover_letter" onChange={handleChange}></input><br></br>
-          <label for="Resume">Resume</label>
-          <input type="checkbox" value={editForm.resume ? false : true} name="resume" onChange={handleChange}></input><br></br>
-          <input type="submit" value="UPDATE" />
+          <h1>Applied</h1>
+            <label for="Apply">Done</label>
+            <input type="checkbox" value={true} name="applied" onChange={handleChange}></input><br></br>
+            <label for="Apply">Not done</label>
+            <input type="checkbox" value={false} name="applied" onChange={handleChange}></input><br></br>
+          <h1>Interviewed</h1>
+            <label for="Interview">Done</label>
+            <input type="checkbox" value={true} name="interviewed" onChange={handleChange}></input><br></br>
+            <label for="Interview">Not done</label>
+            <input type="checkbox" value={false} name="interviewed" onChange={handleChange}></input><br></br>
+          <h1>Cover Letter</h1>
+            <label for="Cover_letter">Done</label>
+            <input type="checkbox" value={true} name="cover_letter" onChange={handleChange}></input><br></br>
+            <label for="Cover_letter">Not done</label>
+            <input type="checkbox" value={false} name="cover_letter" onChange={handleChange}></input><br></br>
+          <h1>Resume</h1>
+            <label for="Resume">Done</label>
+            <input type="checkbox" value={true} name="resume" onChange={handleChange}></input><br></br>
+            <label for="Resume">Not done</label>
+            <input type="checkbox" value={false} name="resume" onChange={handleChange}></input><br></br>
+            <input type="submit" value="UPDATE" />
         </form>
       </div>
     );
